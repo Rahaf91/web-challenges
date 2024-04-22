ff5cf2790f0385ffd4182923056860e9c7267a3d
Rahaf <rahafsekny@gmail.com>
2024-04-22 18:31:26 +0200

    Solve js function basics

---
+++ js-functions_basic-functions/js/index.js
@@ -0,0 +1,56 @@
+console.clear();
+
+/*
+1: Create the data for a book in an online store. Create variables for the following data:
+  - the book title
+  - the author
+  - the rating
+  - the number of sales
+*/
+
+// --v-- write your code here --v--
+let bookTitle = "The Wuthering Heights";
+let bookAuthor = "Emily Bronte";
+let bookRating = 4.5;
+let bookSales = 10000;
+// --^-- write your code here --^--
+
+/*
+2: Log all variables to the console, for example:
+
+Title: The Lord of the Javascript
+Author: Mario
+Rating: 4.2
+Sales: 120
+
+Increase the number of sales and change the rating of the book afterwards.
+Then log all variables to the console again.
+*/
+
+// --v-- write your code here --v--
+logBookData();
+
+bookRating = bookRating + 0.4;
+bookSales += 100;
+logBookData();
+// --^-- write your code here --^--
+bookSales += 200;
+logBookData();
+bookSales += 500;
+logBookData();
+/*
+3: This code is quite repetitive and hard to read.
+ - Write a function called logBookData which console logs all
+   variables when it is called.
+ - Then replace the console logs above with this function.
+ - Now increase the number of sales 2 more times and log the data after each increase.
+*/
+
+// --v-- write your code here --v--
+function logBookData() {
+  console.log(bookTitle);
+  console.log(bookAuthor);
+  console.log(bookRating);
+  console.log(bookSales);
+}
+// --^-- write your code here --^--
