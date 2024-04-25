088dc1ccb46e20f08f6a5c777e774d7d4b148753
Rahaf <rahafsekny@gmail.com>
2024-04-25 13:43:07 +0200

    update

--- js-inputs-and-strings_input-to-uppercase/js/index.js
+++ js-inputs-and-strings_input-to-uppercase/js/index.js
@@ -12,20 +12,6 @@ Hint: Access the value by using `.value` on the input element
 */
 const inputElement = document.querySelector('[data-js="first-input"]');
 const buttonElement = document.querySelector('[data-js="button-uppercase"]');
-
-/*function turnToUppercase() {
-  const inputValue = inputElement.value;
-  const upperCase = inputValue.toUpperCase();
-  inputElement.value = upperCase;
-}
-buttonElement.addEventListener("click", turnToUppercase);*/
-
 buttonElement.addEventListener("click", () => {
-  const inputValue = inputElement.value;
-  const upperCase = inputValue.toUpperCase();
-  inputElement.value = upperCase;
+  inputElement.value = inputElement.value.toUpperCase();
 });
-
-/*buttonElement.addEventListener("click", () => {
-    inputElement.value =inputElement.value.toUpperCase(); 
-})*/
