03b52370f15b0e466527efd2afc44f45bf74770c
Rahaf <rahafsekny@gmail.com>
2024-05-21 12:33:07 +0200

    Solve the challenges

---
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -0,0 +1,14 @@
+console.clear();
+
+const url = "https://swapi.py4e.com/api/people";
+
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  console.log(data.count);
+  console.log(data.next);
+  console.log(data.results[2].eye_color);
+}
+
+fetchData();
