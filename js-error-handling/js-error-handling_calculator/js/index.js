2789b7ec6bf64001188e77ef782077c535bd97e5
Rahaf <rahafsekny@gmail.com>
2024-05-22 17:59:29 +0200

    Solve the challenges

---
+++ js-error-handling/js-error-handling_calculator/js/index.js
@@ -0,0 +1,34 @@
+console.clear();
+
+const form = document.querySelector("form");
+const output = form.querySelector("output");
+const errorMessage = document.querySelector('[data-js="error-message"]');
+
+const operations = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => {
+    if (b === 0) {
+      throw new Error("Cannot divide by zero!");
+    }
+    return a / b;
+  },
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const firstNumber = Number(event.target.firstNumber.value);
+  const secondNumber = Number(event.target.secondNumber.value);
+  const operation = event.target.operation.value;
+  try {
+    output.innerText = operations[operation](firstNumber, secondNumber);
+    errorMessage.innerText = "";
+    form.reset();
+    form.firstNumber.focus();
+  } catch (error) {
+    errorMessage.innerText = `${error} Please add another number`;
+    form.reset();
+    form.firstNumber.focus();
+  }
+});
