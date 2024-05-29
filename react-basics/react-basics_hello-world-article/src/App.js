9e18a50a5f8410ad0dfdc92fe63f798d3ea678ea
Rahaf <rahafsekny@gmail.com>
2024-05-29 12:46:58 +0200

    Solve challenges

---
+++ react-basics/react-basics_hello-world-article/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return <HelloWorldArticle />;
+}
+
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Heading</h1>
+      <p>Hi...I am Rahaf </p>
+    </article>
+  );
+}
---
+++ react-basics/react-basics_hello-world/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return <HelloWorld />;
+}
+
+function HelloWorld() {
+  return (
+    <main>
+      <h1>Hello World!</h1>
+    </main>
+  );
+}
