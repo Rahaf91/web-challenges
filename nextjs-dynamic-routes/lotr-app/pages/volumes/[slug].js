8cebe13354aca15f50f047f8418fdabeabd95d59
Rahaf <rahafsekny@gmail.com>
2024-06-27 11:01:53 +0200

    solve challenge

---
+++ nextjs-dynamic-routes/lotr-app/pages/volumes/[slug].js
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import { volumes } from "../../lib/data";
+import { useRouter } from "next/router";
+import Image from "next/image";
+
+export default function VolumeDetail() {
+  const router = useRouter();
+  const { slug } = router.query;
+
+  const volumeIndex = volumes.findIndex(
+    ({ slug: volumeSlug }) => volumeSlug === slug
+  );
+  const volume = volumes[volumeIndex];
+
+  if (!volume) {
+    return <div>Volume not found</div>;
+  }
+  const prevVolume = volumeIndex > 0 && volumes[volumeIndex - 1];
+  const nextVolume =
+    volumeIndex < volumes.length - 1 && volumes[volumeIndex + 1];
+
+  return (
+    <div>
+      <Link href="/volumes">← All Volumes</Link>
+      <h1>{volume.title}</h1>
+      <p>{volume.description}</p>
+      <ul>
+        {volume.books.map((book) => (
+          <li key={book.ordinal}>
+            {book.ordinal}: {book.title}
+          </li>
+        ))}
+      </ul>
+      <Image
+        src={volume.cover}
+        alt={`${volume.title} cover`}
+        width={140}
+        height={230}
+      />
+      <div>
+        {prevVolume && <Link href={`/volumes/${prevVolume.slug}`}>← Prev</Link>}
+        <br />
+        {nextVolume && <Link href={`/volumes/${nextVolume.slug}`}>next →</Link>}
+      </div>
+    </div>
+  );
+}
---
+++ nextjs-dynamic-routes/lotr-app/pages/volumes/index.js
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { introduction, volumes } from "../../lib/data";
+import { useRouter } from "next/router";
+
+export default function Volumes() {
+  const router = useRouter();
+
+  function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+  return (
+    <div>
+      <Link href="/">Home</Link>
+      <h1>Lord of the Rings</h1>
+      <p>{introduction}</p>
+      <h2>All Volumes</h2>
+      <ul>
+        {volumes.map((volume) => (
+          <li key={volume.slug}>
+            <Link href={`/volumes/${volume.slug}`}>{volume.title}</Link>
+          </li>
+        ))}
+      </ul>
+      <button
+        type="button"
+        onClick={() => {
+          const randomVolume = getRandomElement();
+          router.push(`/volumes/${randomVolume.slug}`);
+        }}
+      >
+        Go to Random Page
+      </button>
+    </div>
+  );
+}
